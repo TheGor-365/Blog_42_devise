c4a413849774683107a0b8f6469def56da242a5b
Gor <7495500@gmail.com>
2021-01-24 12:56:17 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get '/' => 'home#index'
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
+  #get 'contacts' => 'contacts#new'
+  resource :contacts, only: [ :new, :create ], path_names: { :new => '' }
+  
+  resources :articles do
+    resources :comments
+  end
+end
